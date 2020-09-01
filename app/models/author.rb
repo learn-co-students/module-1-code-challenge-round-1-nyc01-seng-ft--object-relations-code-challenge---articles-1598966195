987e3efd8765490946e409544161b7aa54cd5b0a
github username <github email address>
2020-09-01 09:59:46 -0400

    second commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -23,6 +23,15 @@ class Author
     articles.map {|article| article.magazine}.uniq
   end 
 
+  #Given a magazine (as Magazine instance) and a title (as a string), creates a new Article instance and associates it with that author and that magazine.
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end 
+  
+  #Returns a **unique** array of strings with the categories of the magazines the author has contributed to
+  def topic_areas
+    self.articles.map {|article| article.magazine.category}.uniq
+  end 
 
 end
 
