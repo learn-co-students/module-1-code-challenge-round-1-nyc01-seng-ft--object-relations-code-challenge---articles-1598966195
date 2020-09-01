f0fc1e26c6d8a1c5f0567ed7684ab9056a4d3f94
AB <austinbuhler@gmail.com>
2020-09-01 10:57:49 -0400

    Submission

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -25,6 +25,9 @@ class Magazine
     self.articles.map { |articles| articles.title }
   end
 
+  #need to find the article count for each contributor and return if they've written more than twice for the mag
+  ## how to ensure #articles is calling on the same mag instance?
+
   def contributing_authors
     self.contributors.filter { |contributors| contributors.articles.count > 2 }
   end
