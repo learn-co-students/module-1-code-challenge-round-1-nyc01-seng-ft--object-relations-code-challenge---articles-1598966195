f793ab77d05cd8e14d3de1bd3a73283c848712a5
jdhofmann <justindhofmann@gmail.com>
2020-09-01 10:00:17 -0400

    init + built out methods

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,24 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|mag| mag.name == name}
+  end
+
+  def article_titles
+    
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,11 +7,18 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+skully = Author.new('skully')
+blue_bottle = Author.new('blue bottle')
+
+pry = Magazine.new('Pry Magazine', 'binding.pry')
+pink = Magazine.new('pinky', 'salt lamps')
 
 
-
-
-
+art1 = Article.new(skully, pry, 'should have gone running')
+art2 = Article.new(blue_bottle, pink, 'coffee')
+art3 = Article.new(blue_bottle, pry, 'water')
+art4 = Article.new(blue_bottle, pry, 'isdaoigfidgf')
+art5 = Article.new(skully, pink, 'shelter')
 
 
 
