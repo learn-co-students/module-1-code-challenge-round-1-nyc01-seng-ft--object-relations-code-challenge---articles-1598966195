ffe293d06d20a0320884fa93d596207305ac9319
aimee-liang <aimeeeliang@gmail.com>
2020-09-01 10:52:16 -0400

    Final version :/

--- tools/console.rb
+++ tools/console.rb
@@ -7,21 +7,21 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
-author1 = Author.new("Aimee")
-author2 = Author.new("George R.R. Martin")
-author3 = Author.new("Stephen King")
+# author1 = Author.new("Aimee")
+# author2 = Author.new("George R.R. Martin")
+# author3 = Author.new("Stephen King")
 
 
 
-mag1 = Magazine.new("Vogue", "fashion")
-mag2 = Magazine.new("Town and Country", "lifestyle")
-mag3 = Magazine.new("Horror Stories", "writing")
+# mag1 = Magazine.new("Vogue", "fashion")
+# mag2 = Magazine.new("Town and Country", "lifestyle")
+# mag3 = Magazine.new("Horror Stories", "writing")
 
 
-art1 = Article.new(author1, mag1, "Sample")
-art2 = Article.new(author1, mag2, "Fancy homes I cannot afford")
-art3 = Article.new(author2, mag3, "Night Walkers R Real")
-art4 = Article.new(author3, mag2, "Walking Tour of My Yacht")
+# art1 = Article.new(author1, mag1, "Sample Writing Here")
+# art2 = Article.new(author1, mag2, "Fancy homes I cannot afford")
+# art3 = Article.new(author2, mag3, "White Walkers R Real")
+# art4 = Article.new(author3, mag2, "Walking Tour of My Yacht")
 
 
 ### DO NOT REMOVE THIS
