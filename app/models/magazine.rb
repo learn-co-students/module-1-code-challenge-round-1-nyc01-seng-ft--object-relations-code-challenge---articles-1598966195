03c96128dae8e03005c48d5c510d43522209f694
gabrielhicks <gabrielhicks@mail.com>
2020-09-01 10:54:53 -0400

    a little fun

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,5 +1,5 @@
 class Magazine
-  
+
   attr_accessor :name, :category
   #   - The name and category of the magazine **can be** changed after being initialized.
 
@@ -21,6 +21,7 @@ class Magazine
 
   def all_contributions
     # Returns an array of Author instances who have written for this magazine
+    p "Here are all of our contributors, and each of their contributions:"
     Article.all.select {|article| article.magazine == self}.map {|article| article.author}
   end
 
@@ -31,17 +32,20 @@ class Magazine
 
   def self.find_by_name(name)
     # - Given a string of magazine's name, this method returns the first magazine object that matches
+    p "This is the first magazine of that name we could find:"
     self.all.find {|magazine| magazine.name == name}
   end
 
   def article_titles
     # - Returns an array strings of the titles of all articles written for that magazine
+    p "The articles written for this magazine are:"
     Article.all.select {|article| article.magazine == self}.map {|article| article.title}
   end
 
   def contributing_authors
     #   - Returns an array of authors who have written more than 2 articles for the magazine
     author = self.all_contributions
+    p "Here are our contributing authors:"
     author.filter {|author| author.magazine_articles(self) > 2}.uniq
   end
 
