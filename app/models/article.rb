66adcc38f0d22a26635c3cf23fc3a525fa1e209a
gabrielhicks <gabrielhicks@mail.com>
2020-09-01 09:30:09 -0400

    initial commit with readme changed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,28 @@
 class Article
+    attr_reader :author, :magazine, :title
 
+    @@all = []
 
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+        
+    end
 end
+
+# - `Article#initialize(author, magazine, title)`
+#   - An article is initialized with an author as an Author object, a magazine as a Magazine object, and title as a string.
+#   - An article **cannot** change its author, magazine, or title after it is has been initialized.
+# - `Article#title`
+#   - Returns the title for that given article
+# - `Article.all`
+#   - Returns an array of all Article instances
+
+#### Article
+
+# - `Article#author`
+#   - Returns the author for that given article
+# - `Article#magazine`
+#   - Returns the magazine for that given article
