ac5ccd466f2c891e81a9777cada4ab3ff2f9b2ba
aimee-liang <aimeeeliang@gmail.com>
2020-09-01 10:28:01 -0400

    Working on author deliverables

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,7 +1,6 @@
 class Magazine
   attr_accessor :name, :category
 
-  # class variable
   @@all = []
 
   def initialize(name, category)
@@ -22,12 +21,12 @@ class Magazine
     self.all.find(name)
   end
 
-  # article titles should return array of strings of the titles for all articles
+  # iterate through all articles, elements converted to a string, into new arr
   def article_titles
-    Articles.all.map {}
+    Articles.all.map{|articles| article.title.to_s}
   end
 
-    # try to access the class itself, the contributors method, and find all author instances
+    # try to access the class itself, then contributors method, and find all author instances
       #contributed 2x
   def contributing_authors
     self.contributors.find_all{|name| author.count(name) > 2}
