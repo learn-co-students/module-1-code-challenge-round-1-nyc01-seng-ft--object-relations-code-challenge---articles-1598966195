0eb94b60169a69bbe2ee6cd36cc42ea78aff5523
jdhofmann <justindhofmann@gmail.com>
2020-09-01 10:54:48 -0400

    all methods checked

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -13,12 +13,38 @@ class Magazine
     @@all
   end
 
+  def contributors 
+    articles.map{|art| art.author}
+  end
+
   def self.find_by_name(name)
     self.all.find{|mag| mag.name == name}
   end
 
+  def articles 
+    Article.all.filter{|art| art.magazine == self}
+  end
+
   def article_titles
-    
+    articles.map{|art| art.title}
+  end
+
+  def contributing_authors
+    authors_count = {}
+    articles.map do |art|
+      if authors_count.key?(art.author)
+        authors_count[art.author] += 1
+      else
+        authors_count[art.author] = 1
+      end
+    end
+    authors = []
+    authors_count.each do |key, value|
+      if value > 2
+        authors << key
+      end
+    end
+    authors
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,7 @@ end
 
 skully = Author.new('skully')
 blue_bottle = Author.new('blue bottle')
+bob = Author.new('bob')
 
 pry = Magazine.new('Pry Magazine', 'binding.pry')
 pink = Magazine.new('pinky', 'salt lamps')
@@ -19,6 +20,13 @@ art2 = Article.new(blue_bottle, pink, 'coffee')
 art3 = Article.new(blue_bottle, pry, 'water')
 art4 = Article.new(blue_bottle, pry, 'isdaoigfidgf')
 art5 = Article.new(skully, pink, 'shelter')
+art6 = Article.new(blue_bottle, pry, '9595959595959')
+art34 = Article.new(bob, pry, 'aaa')
+art8 = Article.new(bob, pry, 'j')
+art9 = Article.new(bob, pry, 'k')
+art7 = Article.new(bob, pry, 'h')
+art22 = Article.new(bob, pry, 't')
+
 
 
 
