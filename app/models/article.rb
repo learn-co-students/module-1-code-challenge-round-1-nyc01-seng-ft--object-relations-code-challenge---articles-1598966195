37335c2de0607149050f0a899ec45a4831aaa171
gerardmi24 <gerardmistretta@yahoo.com>
2020-09-01 10:15:38 -0400

    Progress report

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,34 @@
 class Article
     #reader - cannot change
+    attr_accessor :article
     attr_reader :name, :author, :magazine, @title
+    @@all = []
 
-    def initialize (name, author, magazine, title)
+    def initialize (name, article, author, magazine, title)
         @name = name
+        @article = article
         @author = author
         @magazine = magazine
         @title = title
+        @@all << self
     end
+
+    def title
+        #Returns the title for that given article
+        if title.article == self
+            article
+        end
+    end
+
+    def self.all
+        #Returns an array of all Article instances
+        @@all
+    end
+
+    def author
+        #Returns the author for that given article
+        Article.all.select {|author| author.article == self}
+    end
+
 
 end
