6357c487d4ad64be199a5657f4f1a24fc1b8d988
aimee-liang <aimeeeliang@gmail.com>
2020-09-01 09:59:59 -0400

    Third commit - working on author and magazine methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -11,13 +11,22 @@ class Author
   end
 
   #articles method should return array of article instances for author
+  def articles
+    Articles.all.select{|article| article.author == self}
+  end
 
   # magazines method should return a unique array of Mags instances author has contributed
-def magazine
+  def magazine
+    Articles.all.select{|article| article.}
+  end
+  # add article method is given two args which will create a new article --> come back to this
+  def add_article(magazine_instance, title_instance)
+    add_article = Article.new(self, magazine_instance, title_instance)
+    article.author == self
+  end
 
-  # add article method is given two args which will create a new article
-
-  # topic areas method will return a unique array of strings of the categories of the mags
+  # topic areas method returns a unique array of strings of the categories of the mags
+    # use to_s method?
 
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -10,5 +10,12 @@ class Magazine
     @@all << self
   end
 
+  # contributors should return array of author instances who wrote for this magazine
+
+  # class method find by name should return the FIRST magazine object that matches
+
+  # instance method article titles should return array of strings of the titles for all articles
+
+  # instance method contributing authors returns array of authors who wrote more than 2x 
 
 end
